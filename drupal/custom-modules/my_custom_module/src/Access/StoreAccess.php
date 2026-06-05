303e9c36feb37d2f606f6ae7ceb5ec8ea70d41e1
suresh-kumara-gist <kumara.suresh25@gmail.com>
2026-06-05 21:47:48 +0530

    issue fix

--- drupal/custom-modules/my_custom_module/src/Access/StoreAccess.php
+++ drupal/custom-modules/my_custom_module/src/Access/StoreAccess.php
@@ -3,23 +3,30 @@
 namespace Drupal\my_custom_module\Access;
 
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\commerce_store\Entity\StoreInterface;
 use Drupal\my_custom_module\BuyerStoreResolverInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class StoreAccess {
+class StoreAccess implements ContainerInjectionInterface {
 
   public function __construct(
     protected BuyerStoreResolverInterface $resolver,
   ) {}
+
+  public static function create(ContainerInterface $container): self {
+    return new self(
+      $container->get('my_custom_module.buyer_store_resolver')
+    );
+  }
 
   public function access(
     StoreInterface $commerce_store,
     AccountInterface $account
   ): AccessResult {
 
-    // Admins bypass store restrictions.
-    if (in_array('administrator', $account->getRoles())) {
+    if (in_array('administrator', $account->getRoles(), TRUE)) {
       return AccessResult::allowed();
     }
 
@@ -27,8 +34,7 @@ class StoreAccess {
 
     return AccessResult::allowedIf(
       in_array($commerce_store->id(), $allowed)
-    )
-      ->cachePerUser();
+    )->cachePerUser();
   }
 
 }
