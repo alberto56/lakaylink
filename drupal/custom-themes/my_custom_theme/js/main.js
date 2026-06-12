fb086ef48137d26b70edc9f2552ffb275022812b
suresh-kumara-gist <kumara.suresh25@gmail.com>
2026-06-12 14:30:19 +0530

    linting fix

--- drupal/custom-themes/my_custom_theme/js/main.js
+++ drupal/custom-themes/my_custom_theme/js/main.js
@@ -10,11 +10,9 @@
         }, 1);
     };
     spinner(0);
-    
-    
+
     // Initiate the wowjs
     new WOW().init();
-
 
     // Sticky Navbar
     $(window).scroll(function () {
@@ -25,139 +23,22 @@
         }
     });
 
-
-    // Hero Header carousel
-    // $(".header-carousel").owlCarousel({
-    //     items: 1,
-    //     autoplay: true,
-    //     smartSpeed: 2000,
-    //     center: false,
-    //     dots: false,
-    //     loop: true,
-    //     margin: 0,
-    //     nav : true,
-    //     navText : [
-    //         '<i class="bi bi-arrow-left"></i>',
-    //         '<i class="bi bi-arrow-right"></i>'
-    //     ]
-    // });
-
-
-    // ProductList carousel
-    // $(".productList-carousel").owlCarousel({
-    //     autoplay: true,
-    //     smartSpeed: 2000,
-    //     dots: false,
-    //     loop: true,
-    //     margin: 25,
-    //     nav : true,
-    //     navText : [
-    //         '<i class="fas fa-chevron-left"></i>',
-    //         '<i class="fas fa-chevron-right"></i>'
-    //     ],
-    //     responsiveClass: true,
-    //     responsive: {
-    //         0:{
-    //             items:1
-    //         },
-    //         576:{
-    //             items:1
-    //         },
-    //         768:{
-    //             items:2
-    //         },
-    //         992:{
-    //             items:2
-    //         },
-    //         1200:{
-    //             items:3
-    //         }
-    //     }
-    // });
-
-    // ProductList categories carousel
-    // $(".productImg-carousel").owlCarousel({
-    //     autoplay: true,
-    //     smartSpeed: 1500,
-    //     dots: false,
-    //     loop: true,
-    //     items: 1,
-    //     margin: 25,
-    //     nav : true,
-    //     navText : [
-    //         '<i class="bi bi-arrow-left"></i>',
-    //         '<i class="bi bi-arrow-right"></i>'
-    //     ]
-    // });
-
-
-    // Single Products carousel
-    // $(".single-carousel").owlCarousel({
-    //     autoplay: true,
-    //     smartSpeed: 1500,
-    //     dots: true,
-    //     dotsData: true,
-    //     loop: true,
-    //     items: 1,
-    //     nav : true,
-    //     navText : [
-    //         '<i class="bi bi-arrow-left"></i>',
-    //         '<i class="bi bi-arrow-right"></i>'
-    //     ]
-    // });
-
-
-    // // ProductList carousel
-    // $(".related-carousel").owlCarousel({
-    //     autoplay: true,
-    //     smartSpeed: 1500,
-    //     dots: false,
-    //     loop: true,
-    //     margin: 25,
-    //     nav : true,
-    //     navText : [
-    //         '<i class="fas fa-chevron-left"></i>',
-    //         '<i class="fas fa-chevron-right"></i>'
-    //     ],
-    //     responsiveClass: true,
-    //     responsive: {
-    //         0:{
-    //             items:1
-    //         },
-    //         576:{
-    //             items:1
-    //         },
-    //         768:{
-    //             items:2
-    //         },
-    //         992:{
-    //             items:3
-    //         },
-    //         1200:{
-    //             items:4
-    //         }
-    //     }
-    // });
-
-
-
     // Product Quantity
     $('.quantity button').on('click', function () {
         var button = $(this);
         var oldValue = button.parent().parent().find('input').val();
+        var newVal;
         if (button.hasClass('btn-plus')) {
-            var newVal = parseFloat(oldValue) + 1;
+          newVal = parseFloat(oldValue) + 1;
         } else {
-            if (oldValue > 0) {
-                var newVal = parseFloat(oldValue) - 1;
-            } else {
-                newVal = 0;
-            }
+          if (oldValue > 0) {
+            newVal = parseFloat(oldValue) - 1;
+          } else {
+            newVal = 0;
+          }
         }
         button.parent().parent().find('input').val(newVal);
     });
-
-
     
    // Back to top button
    $(window).scroll(function () {
@@ -171,9 +52,4 @@
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
     });
-
-
-   
-
 })(jQuery);
-
