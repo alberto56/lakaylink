6685f275f8ac1fcda1becceaea3d6f3b38f50693
suresh-kumara-gist <kumara.suresh25@gmail.com>
2026-08-17 20:18:36 +0530

    append-language-code

--- drupal/custom-modules/my_custom_module/src/Controller/LoginController.php
+++ drupal/custom-modules/my_custom_module/src/Controller/LoginController.php
@@ -6,6 +6,8 @@ namespace Drupal\my_custom_module\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Block\BlockManagerInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -21,10 +23,26 @@ final class LoginController extends ControllerBase {
   protected $blockManager;
 
   /**
-   * Constructor.
+   * The language manager.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
    */
-  public function __construct(BlockManagerInterface $block_manager) {
+  protected $languageManager;
+
+  /**
+   * Constructs the controller.
+   *
+   * @param \Drupal\Core\Block\BlockManagerInterface $block_manager
+   *   The block manager.
+   * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
+   *   The language manager.
+   */
+  public function __construct(
+    BlockManagerInterface $block_manager,
+    LanguageManagerInterface $language_manager,
+  ) {
     $this->blockManager = $block_manager;
+    $this->languageManager = $language_manager;
   }
 
   /**
@@ -32,7 +50,8 @@ final class LoginController extends ControllerBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('plugin.manager.block')
+      $container->get('plugin.manager.block'),
+      $container->get('language_manager'),
     );
   }
 
@@ -40,16 +59,21 @@ final class LoginController extends ControllerBase {
    * Custom login page.
    */
   public function loginPage() {
+    $language = $this->languageManager->getCurrentLanguage();
 
-    // Create Social Auth Login block.
-    $plugin_block = $this->blockManager->createInstance('social_auth_login', []);
-
-    // Build render array.
-    $social_login_block = $plugin_block->build();
+    // Google Social Auth login endpoint.
+    $google_login_url = Url::fromUserInput(
+      '/user/login/google',
+      [
+        'query' => [
+          'language' => $language->getId(),
+        ],
+      ]
+    )->toString();
 
     return [
       '#theme' => 'custom_login_page',
-      '#social_login_block' => $social_login_block,
+      '#google_login_url' => $google_login_url,
     ];
   }
 
