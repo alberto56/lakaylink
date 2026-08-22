6ed4da3439a00a278225552d632458021fed5a45
suresh-kumara-gist <kumara.suresh25@gmail.com>
2026-08-22 16:09:50 +0530

    fixed all phpstan issues

---
+++ tests/browser-tests/testAnonymousUser.js
@@ -0,0 +1,30 @@
+const { expect } = require('chai')
+const fs = require('fs')
+const testBase = require('./testBase.js')
+
+
+it('Anonymous user should see Login by google button', async function() {
+    this.timeout(25000);
+    const puppeteer = require('puppeteer')
+    const browser = await puppeteer.launch({
+       headless: true,
+       args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
+    var result = false
+    const page = await browser.newPage()
+    try {
+      console.log('Testing ' + __filename)
+      console.log('set viewport')
+      await page.setViewport({ width: 1280, height: 800 })
+      console.log('go to the login page')
+      await page.goto('http://webserver')
+
+      await testBase.screenshot(page, 'custom login page', await page.content());
+      await testBase.assertInSourceCode(page, 'Sign in With Google')
+
+    }
+    catch (error) {
+      await testBase.showError(error, browser, page);
+    }
+    await browser.close()
+  });
