2eaa5513605da1b6e5cdc9494723213819c337a5
suresh-kumara-gist <kumara.suresh25@gmail.com>
2026-08-23 15:02:19 +0530

    issue fix

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -18,12 +18,33 @@ echo "Test running environment"
 ./scripts/test-running-environment.sh
 
 echo "======= Before End-to-end tests ======="
-ls -la
+ls -la  /home/runner/work/lakaylink/lakaylink/do-not-commit/
+ls -la  /home/runner/work/lakaylink/lakaylink/do-not-commit/screenshots/
+ls -la  /home/runner/work/lakaylink/lakaylink/do-not-commit/dom-captures/
+
 echo "End-to-end tests"
 ./scripts/end-to-end-tests.sh
 
+echo "======= After End-to-end tests ======="
+ls -la  /home/runner/work/lakaylink/lakaylink/do-not-commit/
+ls -la  /home/runner/work/lakaylink/lakaylink/do-not-commit/screenshots/
+ls -la  /home/runner/work/lakaylink/lakaylink/do-not-commit/dom-captures/
+
+
+echo "======= before Accessibility tests ======="
+ls -la  /home/runner/work/lakaylink/lakaylink/do-not-commit/
+ls -la  /home/runner/work/lakaylink/lakaylink/do-not-commit/screenshots/
+ls -la  /home/runner/work/lakaylink/lakaylink/do-not-commit/dom-captures/
+
 echo "Accessibility tests"
 ./scripts/a11y-tests.sh
+
+echo "======= after Accessibility tests ======="
+ls -la  /home/runner/work/lakaylink/lakaylink/do-not-commit/
+ls -la  /home/runner/work/lakaylink/lakaylink/do-not-commit/screenshots/
+ls -la  /home/runner/work/lakaylink/lakaylink/do-not-commit/dom-captures/
+
+
 
 BACKUPID="$(./scripts/lib/data-unique-dirname.sh)"
 echo "Export test to $BACKUPID"
