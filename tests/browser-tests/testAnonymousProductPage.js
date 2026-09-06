c485e756d541080e710ac6e0c3df3987c152a22b
suresh-kumara-gist <kumara.suresh25@gmail.com>
2026-09-06 16:52:41 +0530

    issue fix

--- tests/browser-tests/testAnonymousProductPage.js
+++ tests/browser-tests/testAnonymousProductPage.js
@@ -2,7 +2,7 @@ const { expect } = require('chai')
 const fs = require('fs')
 const testBase = require('./testBase.js')
 
-it('Anonymous user should see Login to buy button', async function() {
+it('Anonymous user product page redirects to custom login', async function() {
   this.timeout(25000);
   const puppeteer = require('puppeteer')
   const browser = await puppeteer.launch({
@@ -18,8 +18,8 @@ it('Anonymous user should see Login to buy button', async function() {
     console.log('go to the login page')
     await page.goto('http://webserver/test-product')
 
-    await testBase.screenshot(page, 'product page', await page.content());
-    await testBase.assertInSourceCode(page, 'Login to buy')
+    await testBase.screenshot(page, 'custom login page', await page.content());
+    await testBase.assertInSourceCode(page, 'Sign in With Google')
 
   }
   catch (error) {
@@ -28,29 +28,64 @@ it('Anonymous user should see Login to buy button', async function() {
   await browser.close()
 });
 
-
-it('Anonymous user should see Login by google button', async function() {
+it('Anonymous user should access products api', async function () {
   this.timeout(25000);
-  const puppeteer = require('puppeteer')
+
+  const puppeteer = require('puppeteer');
+
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
-  })
-  var result = false
-  const page = await browser.newPage()
+  });
+
+  const page = await browser.newPage();
+
   try {
-    console.log('Testing ' + __filename)
-    console.log('set viewport')
-    await page.setViewport({ width: 1280, height: 800 })
-    console.log('go to the login page')
-    await page.goto('http://webserver')
+    console.log('Testing ' + __filename);
 
-    await testBase.screenshot(page, 'product page', await page.content());
-    await testBase.assertInSourceCode(page, 'Authenticate through Google')
+    await page.setViewport({
+      width: 1280,
+      height: 800
+    });
 
-  }
-  catch (error) {
+    const apiUrl =
+      'http://webserver/api/product/grocery?include=variations';
+
+    const response = await page.goto(apiUrl);
+
+    expect(response.status()).to.equal(200);
+
+    const json = await response.json();
+
+    const { data = [], included = [] } = json;
+
+    console.log(`Products: ${data.length}`);
+    console.log(`Included entities: ${included.length}`);
+
+    // Products should not be empty.
+    testBase.assertNotEmpty(
+      data,
+      'Products API should return products'
+    );
+
+    // Get grocery variations from included entities.
+    const variations = included.filter(
+      item =>
+        item.type === 'commerce_product_variation--grocery_variation'
+    );
+
+    console.log(`Variations: ${variations.length}`);
+
+    // Variations should not be empty.
+    testBase.assertNotEmpty(
+      variations,
+      'Products API should return product variations'
+    );
+
+  } catch (error) {
     await testBase.showError(error, browser, page);
+    throw error;
+  } finally {
+    await browser.close();
   }
-  await browser.close()
 });
