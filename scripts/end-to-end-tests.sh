6b0f2e6cce25814a0cb4691397493cf4c75b0917
suresh-kumara-gist <kumara.suresh25@gmail.com>
2026-09-11 21:32:41 +0530

    debug

--- scripts/end-to-end-tests.sh
+++ scripts/end-to-end-tests.sh
@@ -12,45 +12,10 @@ echo 'Updating password for admin so our testbot knows how to login'
 docker compose exec -T drupal /bin/bash -c 'drush upwd $(drush uinf --uid=1 --field=name) '"$PASS"
 
 
-DEFAULT_PRODUCT_TITLE="Default Test Product3"
 GROCERY_PRODUCT_TITLE="Grocery Test Product3"
 
 docker compose exec -T drupal drush php:eval "
 \$storage = \Drupal::entityTypeManager()->getStorage('commerce_product');
-
-\$existing = \$storage->loadByProperties(['title' => '$DEFAULT_PRODUCT_TITLE']);
-
-if (!\$existing) {
-  \$product = \Drupal\commerce_product\Entity\Product::create([
-    'type' => 'default',
-    'title' => '$DEFAULT_PRODUCT_TITLE',
-    'stores' => [1],
-    'status' => 1,
-  ]);
-  \$product->save();
-
-  \$variation = \Drupal\commerce_product\Entity\ProductVariation::create([
-    'type' => 'default',
-    'sku' => 'TEST-SKU-0013',
-    'title' => '$DEFAULT_PRODUCT_TITLE Variation',
-    'price' => [
-      'number' => '19.99',
-      'currency_code' => 'USD',
-    ],
-    'product_id' => \$product->id(),
-    'status' => 1,
-  ]);
-  \$variation->save();
-
-  \$product->addVariation(\$variation);
-  \$product->save();
-
-  print 'Product created';
-}
-else {
-  print 'Product already exists';
-}
-
 
 \$existing = \$storage->loadByProperties(['title' => '$GROCERY_PRODUCT_TITLE']);
 
@@ -154,42 +119,6 @@ echo 'change passwords'
 docker compose exec -T drupal /bin/bash -c 'drush upwd test_unverified '"$PASS"
 docker compose exec -T drupal /bin/bash -c 'drush upwd test_seller '"$PASS"
 docker compose exec -T drupal /bin/bash -c 'drush upwd test_buyer '"$PASS"
-
-docker compose exec -T drupal /bin/bash -c 'drush cim -y'
-
-docker compose exec -T drupal /bin/bash -c "drush role:perm:add anonymous 'view commerce_product'"
-docker compose exec -T drupal /bin/bash -c "drush role:perm:add anonymous 'view commerce_product_attribute'"
-docker compose exec -T drupal /bin/bash -c "drush role:perm:add anonymous 'view grocery_product commerce_product'"
-
-
-docker compose exec -T drupal drush php:eval '
-$permissions = \Drupal::service("user.permissions")->getPermissions();
-
-foreach ($permissions as $permission => $info) {
-  if (str_contains($permission, "grocery") || str_contains($permission, "commerce_product")) {
-    print $permission . PHP_EOL;
-  }
-}
-'
-
-
-docker compose exec -T drupal drush php:eval '
-$storage = \Drupal::entityTypeManager()->getStorage("commerce_product");
-
-$products = $storage->loadByProperties([
-  "title" => "Default Test Product3",
-]);
-
-print "Products found: " . count($products) . PHP_EOL;
-
-foreach ($products as $product) {
-  print "ID: " . $product->id() . PHP_EOL;
-  print "Title: " . $product->label() . PHP_EOL;
-  print "Bundle: " . $product->bundle() . PHP_EOL;
-  print "Published: " . ($product->isPublished() ? "YES" : "NO") . PHP_EOL;
-}
-'
-
 
 echo 'Running our tests'
 docker run \
