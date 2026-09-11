dfd4323479990a43a98dbfe8e17d2c59679c355c
suresh-kumara-gist <kumara.suresh25@gmail.com>
2026-09-11 21:22:56 +0530

    debug

--- tests/browser-tests/testAnonymousProductPage.js
+++ tests/browser-tests/testAnonymousProductPage.js
@@ -2,31 +2,31 @@ const { expect } = require('chai')
 const fs = require('fs')
 const testBase = require('./testBase.js')
 
-// it('Anonymous user product page redirects to custom login', async function() {
-//   this.timeout(25000);
-//   const puppeteer = require('puppeteer')
-//   const browser = await puppeteer.launch({
-//      headless: true,
-//      args: ['--no-sandbox', '--disable-setuid-sandbox']
-//   })
-//   var result = false
-//   const page = await browser.newPage()
-//   try {
-//     console.log('Testing ' + __filename)
-//     console.log('set viewport')
-//     await page.setViewport({ width: 1280, height: 800 })
-//     console.log('go to the login page')
-//     await page.goto('http://webserver/test-product')
+it('Anonymous user product page redirects to custom login', async function() {
+  this.timeout(25000);
+  const puppeteer = require('puppeteer')
+  const browser = await puppeteer.launch({
+     headless: true,
+     args: ['--no-sandbox', '--disable-setuid-sandbox']
+  })
+  var result = false
+  const page = await browser.newPage()
+  try {
+    console.log('Testing ' + __filename)
+    console.log('set viewport')
+    await page.setViewport({ width: 1280, height: 800 })
+    console.log('go to the login page')
+    await page.goto('http://webserver/test-product')
 
-//     await testBase.screenshot(page, 'custom login page', await page.content());
-//     await testBase.assertInSourceCode(page, 'Sign in With Google')
+    await testBase.screenshot(page, 'custom login page', await page.content());
+    await testBase.assertInSourceCode(page, 'Sign in With Google')
 
-//   }
-//   catch (error) {
-//     await testBase.showError(error, browser, page);
-//   }
-//   await browser.close()
-// });
+  }
+  catch (error) {
+    await testBase.showError(error, browser, page);
+  }
+  await browser.close()
+});
 
 it('Anonymous user should access products api', async function () {
   this.timeout(25000);
